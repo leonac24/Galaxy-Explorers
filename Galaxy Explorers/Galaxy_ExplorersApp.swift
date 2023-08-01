475a7391bc01d469130215d34ffba6e1f7d4fe50
Leona Chen <leonachenpiano@gmail.com>
2023-08-01 17:44:28 -0400

    Initial Commit

---
+++ Galaxy Explorers/Galaxy_ExplorersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Galaxy_ExplorersApp.swift
+//  Galaxy Explorers
+//
+//  Created by Leona Chen on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Galaxy_ExplorersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
